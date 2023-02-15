5362cc20c3f01f902046a125be99c37201927f68
Marijn Suijten <marijn@traverseresearch.nl>
2023-02-15 13:46:45 +0100

    android: Upgrade to `jni 0.21`

--- src/imp/platform/android.rs
+++ src/imp/platform/android.rs
@@ -12,42 +12,29 @@ impl From<jni::errors::Error> for AppDirsError {
 
 fn get_jni_app_dir(
     context: &jni::objects::JObject<'_>,
-    env: &jni::JNIEnv<'_>,
+    env: &mut jni::JNIEnv<'_>,
     method: &str,
     has_string_argument: bool,
 ) -> Result<String, AppDirsError> {
     let dir = if has_string_argument {
         env.call_method(
-            *context,
+            context,
             method,
             "(Ljava/lang/String;)Ljava/io/File;",
-            &[jni::objects::JValue::Void],
+            &[jni::objects::JValue::Object(&jni::objects::JObject::null())],
         )
     } else {
-        env.call_method(*context, method, "()Ljava/io/File;", &[])
-    }?;
-    let dir = match dir {
-        jni::objects::JValue::Object(o) => o,
-        _ => {
-            return Err(AppDirsError::Io(Error::new(
-                ErrorKind::Other,
-                "dir is not `JObject`",
-            )))
-        },
-    };
+        env.call_method(context, method, "()Ljava/io/File;", &[])
+    }?
+    .l()?;
 
-    let path_string = env.call_method(dir, "getPath", "()Ljava/lang/String;", &[])?;
-    let path_string = match path_string {
-        jni::objects::JValue::Object(o) => jni::objects::JString::from(o),
-        _ => {
-            return Err(AppDirsError::Io(Error::new(
-                ErrorKind::Other,
-                "path_string is not `JObject`",
-            )))
-        },
-    };
+    let path_string = env
+        .call_method(dir, "getPath", "()Ljava/lang/String;", &[])?
+        .l()?;
+    let path_string = jni::objects::JString::from(path_string);
+    let path_string = env.get_string(&path_string)?;
 
-    Ok(env.get_string(path_string)?.into())
+    Ok(path_string.into())
 }
 
 pub fn get_app_dir(t: AppDataType) -> Result<PathBuf, AppDirsError> {
@@ -64,17 +51,17 @@ pub fn get_app_dir(t: AppDataType) -> Result<PathBuf, AppDirsError> {
 
     let android_context = ndk_context::android_context();
     let vm = unsafe { jni::JavaVM::from_raw(android_context.vm().cast()) }?;
-    let env = vm.attach_current_thread()?;
-    let context = jni::objects::JObject::from(android_context.context().cast());
+    let mut env = vm.attach_current_thread()?;
+    let context = unsafe { jni::objects::JObject::from_raw(android_context.context().cast()) };
 
     let path_string = match t {
-        AppDataType::UserConfig => get_jni_app_dir(&context, &env, "getDataDir", false)?,
-        AppDataType::UserData => get_jni_app_dir(&context, &env, "getFilesDir", false)?,
-        AppDataType::UserCache => get_jni_app_dir(&context, &env, "getCacheDir", false)?,
+        AppDataType::UserConfig => get_jni_app_dir(&context, &mut env, "getDataDir", false)?,
+        AppDataType::UserData => get_jni_app_dir(&context, &mut env, "getFilesDir", false)?,
+        AppDataType::UserCache => get_jni_app_dir(&context, &mut env, "getCacheDir", false)?,
         AppDataType::SharedData | AppDataType::SharedConfig => {
-            get_jni_app_dir(&context, &env, "getExternalFilesDir", true)?
+            get_jni_app_dir(&context, &mut env, "getExternalFilesDir", true)?
         },
-        // AppDataType::SharedCache => get_jni_app_dir(&context, &env, "getExternalCacheDir", false)?,
+        // AppDataType::SharedCache => get_jni_app_dir(&context, &mut env, "getExternalCacheDir", false)?,
     };
 
     Ok(PathBuf::from(path_string))
